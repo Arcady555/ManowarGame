680cf5e18d7c81c791ad6770e76cc56fc75c78cf
Arrcady555 <parfenov7233@gmail.com>
2021-10-26 22:31:35 +0300

    Update Manowar

--- manovar/src/Manowar.java
+++ manovar/src/Manowar.java
@@ -1,13 +1,20 @@
 public class Manowar {
-    private final int health = 100;
-    public void strike (Manowar opponent) {
-        System.out.println(this + " strike!");
-    }
 
+    private  int health = 100;
     public int getHealth() {
         return health;
     }
     public String toString() {
         return "man";
+    }
+
+    public void fight(Manowar point1, Manowar point2) {
+        System.out.println(point1 + " strike!");
+        point2.health -= 20;
+        System.out.println("The " + point2 + "'s health is " + point2.health);
+        if (point2.health == 0) {
+            System.out.println(point1 + " win!!!!!");
+            System.exit(0);
+        }
     }
 }
